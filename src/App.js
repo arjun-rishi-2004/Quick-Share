c41817b8afb674a2905e793e87ae670a5fd0043d
Arjun Rishi <128972815+arjun-rishi-2004@users.noreply.github.com>
2023-10-07 19:23:23 +0530

    Quickshare -2

--- src/App.js
+++ src/App.js
@@ -4,31 +4,31 @@ import { db } from "./firebase-config";
 import { collection, getDocs, addDoc, doc, deleteDoc } from "firebase/firestore";
 
 function App() {
-  // States for input and data
   const [sharedText, setSharedText] = useState("");
   const sharedCollectionRef = collection(db, "shared_data");
   const [sharedData, setSharedData] = useState([]);
 
-  // Function to share data
   const shareData = async () => {
     await addDoc(sharedCollectionRef, { text: sharedText, timestamp: new Date() });
-    setSharedText(""); // Reset input after sharing
+    setSharedText("");
   }
 
-  // Function to delete shared data
   const deleteSharedData = async (id) => {
     const sharedDoc = doc(db, "shared_data", id);
     await deleteDoc(sharedDoc);
   }
 
-  // Fetch shared data
+  const copyData = (text) => {
+    navigator.clipboard.writeText(text);
+  }
+
   useEffect(() => {
     const getSharedData = async () => {
       const data = await getDocs(sharedCollectionRef);
       setSharedData(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
     }
     getSharedData();
-  }, []);
+  }, [sharedText]); // Added sharedText to the dependency array
 
   return (
     <div className="App">
@@ -41,10 +41,13 @@ function App() {
       <button onClick={shareData}>Share</button>
       {
         sharedData.map(data => (
-          <div key={data.id}>
+          <div className="shared-data" key={data.id}>
             <p>{data.text}</p>
             <p>Shared on: {data.timestamp.toISOString()}</p>
-            <button onClick={() => deleteSharedData(data.id)}>Delete</button>
+            <div className="button-container">
+              <button onClick={() => deleteSharedData(data.id)} className="delete-button">Delete</button>
+              <button onClick={() => copyData(data.text)} className="copy-button">Copy</button>
+            </div>
           </div>
         ))
       }
