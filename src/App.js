7d60892d060442b679b6b1266644d65c9c290e4d
Arjun Rishi <128972815+arjun-rishi-2004@users.noreply.github.com>
2023-10-27 13:29:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,82 +1,64 @@
-import { useState } from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { db } from "./firebase-config";
+import { collection, getDocs, addDoc, doc, deleteDoc } from "firebase/firestore";
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [authenticated, setAuthenticated] = useState(false);
   const [sharedText, setSharedText] = useState("");
+  const sharedCollectionRef = collection(db, "shared_data");
   const [sharedData, setSharedData] = useState([]);
 
-  const shareData = () => {
-    if (sharedText.trim() !== "") {
-      setSharedData([...sharedData, sharedText]);
-      setSharedText("");
+  const shareData = async () => {
+    await addDoc(sharedCollectionRef, { text: sharedText });
+    setSharedText("");
+  }
+const  getSharedData = async () => { 
+  const data = await getDocs(sharedCollectionRef);   
+  setSharedData(data.docs.map(doc => ({ ...doc.data(), id: doc.id }))); }
+
+  
+  const deleteSharedData = async (id) => {
+    const sharedDoc = doc(db, "shared_data", id);
+    await deleteDoc(sharedDoc);
+    getSharedData();
+  }
+
+  const copyData = (text) => {
+    navigator.clipboard.writeText(text);
+  }
+
+  useEffect(() => {
+    const getSharedData = async () => {
+      const data = await getDocs(sharedCollectionRef);
+      setSharedData(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
     }
-  }
-
-  const deleteSharedData = (index) => {
-    const updatedData = [...sharedData];
-    updatedData.splice(index, 1);
-    setSharedData(updatedData);
-  }
-
-  const handleLogin = () => {
-    if (username === 'arjun' && password === 'password') {
-      setAuthenticated(true);
-    } else {
-      alert('Invalid credentials. Please try again.');
-    }
-  }
-
-  const handleLogout = () => {
-    setAuthenticated(false);
-  }
+    getSharedData();
+  }, [sharedText]);
 
   return (
     <div className="App">
-      {authenticated ? (
-        <>
-          <div className="input-container">
-            <textarea
-              value={sharedText}
-              onChange={(event) => setSharedText(event.target.value)}
-              placeholder="Enter text to share"
-            />
-            <button onClick={shareData}>Share</button>
-            <button onClick={handleLogout}>Logout</button>
+      <div className="input-container">
+        <textarea
+          value={sharedText}
+          onChange={(event) => setSharedText(event.target.value)}
+          placeholder="Enter text to share"
+        />
+        <button onClick={shareData}>Share</button>
+      </div>
+      {
+        sharedData.map(data => (
+          <div className="shared-data" key={data.id}>
+            <pre>{data.text}</pre>
+            <div className="button-container">
+              <button onClick={() => deleteSharedData(data.id)} className="delete-button">Delete</button>
+              <button onClick={() => copyData(data.text)} className="copy-button">Copy</button>
+            </div>
           </div>
-          {
-            sharedData.map((data, index) => (
-              <div className="shared-data" key={index}>
-                <pre>{data}</pre>
-                <div className="button-container">
-                  <button onClick={() => deleteSharedData(index)} className="delete-button">Delete</button>
-                </div>
-              </div>
-            ))
-          }
-        </>
-      ) : (
-        <div className="login-container">
-          <h2>Login</h2>
-          <input 
-            type="text" 
-            placeholder="Username" 
-            value={username} 
-            onChange={(e) => setUsername(e.target.value)} 
-          />
-          <input 
-            type="password" 
-            placeholder="Password" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-          />
-          <button onClick={handleLogin}>Login</button>
-        </div>
-      )}
+        ))
+      }
     </div>
-  );
+  )
 }
 
 export default App;
+  
