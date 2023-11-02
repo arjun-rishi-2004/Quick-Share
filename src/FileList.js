a45a12531b2a5bc03ad9284a02fa3bb555f0726e
arjun-rishi-2004 <arjunrishi.github@gmail.com>
2023-11-02 23:08:24 +0530

    search bar for file done

--- src/FileList.js
+++ src/FileList.js
@@ -1,21 +1,22 @@
-import { useEffect, useState } from "react";
-import { storage,ref } from "./firebase-config";
-import { listAll, getDownloadURL } from "firebase/storage";
+import React, { useState, useEffect } from 'react';
+import { storage, ref } from './firebase-config';
+import { listAll, getDownloadURL } from 'firebase/storage';
 import Button from '@mui/joy/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { deleteObject } from "firebase/storage";
+import { deleteObject } from 'firebase/storage';
 import SendIcon from '@mui/icons-material/Send';
- 
+import { TextField } from '@mui/material';
 
 function FileList() {
   const [files, setFiles] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
     const fetchFiles = async () => {
-      const storageRef = ref(storage, "uploaded_files");
+      const storageRef = ref(storage, 'uploaded_files');
       const filesList = await listAll(storageRef);
       const fileUrls = await Promise.all(
         filesList.items.map(async (item) => ({
@@ -32,35 +33,50 @@ function FileList() {
   const deleteFile = async (fileName) => {
     const storageRef = ref(storage, `uploaded_files/${fileName}`);
     await deleteObject(storageRef);
-    const updatedFiles = files.filter(file => file.name !== fileName);
+    const updatedFiles = files.filter((file) => file.name !== fileName);
     setFiles(updatedFiles);
   };
-
 
   return (
     <div>
       <h2>Uploaded Files</h2>
-      
+   
+      <TextField type="text"
+        placeholder="Search files..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value) }id="outlined-search" label="Search files" />
 
 
+      {files
+        .filter((file) =>
+          file.name.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+        .map((file) => (
+          <Card
+            sx={{ maxWidth: 345, padding: 1, marginTop: 1, float: 'left' }}
+            key={file.name}
+          >
+            {file.name}{' '}
+            <CardActions>
+              <Button
+                variant="outlined"
+                component="a"
+                href={file.downloadUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Download
+              </Button>
 
-        {files.map((file) => (
-                <Card sx={{ maxWidth: 345,padding:1,marginTop:1 ,float:"left"}} key={file.name}>
-            {file.name}{" "}
-            {/* <a href={file.downloadUrl} target="_blank" rel="noopener noreferrer">
-              Download
-            </a> */}
-<CardActions>
-            <Button variant="outlined" component="a" href="#as-link"  href={file.downloadUrl} target="_blank" rel="noopener noreferrer">
-            Download</Button>
-          
-            <Button onClick={() => deleteFile(file.name)} variant="outlined" startcon={<DeleteIcon />}  >
-  Delete
-</Button>
-
-
+              <Button
+                onClick={() => deleteFile(file.name)}
+                variant="outlined"
+                startIcon={<DeleteIcon />}
+              >
+                Delete
+              </Button>
             </CardActions>
-            </Card>
+          </Card>
         ))}
     </div>
   );
